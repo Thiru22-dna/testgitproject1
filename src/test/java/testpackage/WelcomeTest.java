7abcfc926ef9796b09b2fe0b239732f6cce1008a
Thiru22-dnam <thirunavukkarasu16.samidurai>
2022-12-23 12:22:14 +0530

    Added WelcomeTest.java to slavebranch1

--- src/test/java/testpackage/WelcomeTest.java
+++ src/test/java/testpackage/WelcomeTest.java
@@ -3,9 +3,9 @@ package testpackage;
 public class WelcomeTest {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Welcome to Java");
 		
-			}
+		System.out.println("Added Code by SlaveBranch1");
+		}
 
 }
