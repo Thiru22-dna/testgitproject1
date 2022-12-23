2bf013ff4e3c1fc6add1a0e5af443b229bb2062b
Thiru22-dnam <thirunavukkarasu16.samidurai>
2022-12-23 12:56:38 +0530

    Code has updated into slavebranch

--- src/test/java/testpackage/WelcomeTest.java
+++ src/test/java/testpackage/WelcomeTest.java
@@ -3,8 +3,9 @@ package testpackage;
 public class WelcomeTest {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Welcome to Java");
+		
+		System.out.println("Code has updated into slavebranch");
 	}
 
 }
